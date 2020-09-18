fb3a584f9ce3094cf23d4beadf565f04e8faddf7
zhenya <krivorotzena60@gmail.com>
2020-09-18 19:27:39 +0300

    blog-fix-migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,7 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('right')->nullable();
+            $table->string('verify_token')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
